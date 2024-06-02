e7ba253f35ddb94bc6a35953c2383460a22fddad
Jakub Spanier <jakubspanier@gmail.com>
2024-06-02 17:08:50 +0200

    Spanier/refactor convert to pandas (#6)
    * refactor pandas converter
    
    * rename
    
    * refactor split_long_examples

---
+++ converters/pandas_converter.py
@@ -0,0 +1,172 @@
+from dataclasses import dataclass, field
+
+import pandas as pd
+import numpy as np
+import random
+from pathlib import Path
+from typing import Tuple, Final
+
+SEED: Final[int] = 1353
+TRAIN_TO_TEST_DEV_RATIO: Final[float] = 0.8
+DEV_TO_TEST_RATIO: Final[float] = 0.5
+
+
+@dataclass
+class TsvFileData:
+    words: list[list[str]] = field(default_factory=lambda: [[]])
+    tags: list[list[str]] = field(default_factory=lambda: [[]])
+    spaces_after: list[list[str]] = field(default_factory=lambda: [[]])
+
+    def __iter__(self):
+        return iter(zip(self.words, self.tags, self.spaces_after))
+
+    def append(self, word: str, tag: str, space: str) -> None:
+        self.words[-1].append(word)
+        self.tags[-1].append(tag)
+        self.spaces_after[-1].append(space)
+
+    def append_next_lists(self) -> None:
+        self.words.append([])
+        self.tags.append([])
+        self.spaces_after.append([])
+
+    def to_data_frame(self) -> pd.DataFrame:
+        return pd.DataFrame(
+            {
+                "words": word,
+                "labels": tag,
+                "times": space_after,
+                "sentence_id": id_,
+            }
+            for id_, (words, tags, spaces_after) in enumerate(self)
+            for word, tag, space_after in zip(words, tags, spaces_after)
+        )
+
+
+class DataFrameSplitter:
+    def __init__(self, data_file: Path, out_file: Path, split_to_files: bool = False):
+        """
+        Initializes the DataFrameSplitter with the given parameters.
+
+        :param data_file: Path to the input data file.
+        :param out_file: Path to the output file or directory.
+        :param split_to_files: Flag to split data into multiple files.
+        """
+        self.data_file = data_file
+        self.out_file = out_file
+        self.split_to_files = split_to_files
+        self.seed = SEED
+        self.data = TsvFileData()
+        random.seed(SEED)
+        np.random.seed(SEED)
+
+    def run(self):
+        """
+        Runs the data processing and splitting based on initialized parameters.
+        """
+        df = self._process_file(self.data_file)
+
+        if self.split_to_files:
+            train_data, dev_test_data = self._split_dataframe(
+                df, train_prop=TRAIN_TO_TEST_DEV_RATIO
+            )
+            dev_data, test_data = self._split_dataframe(
+                dev_test_data, train_prop=DEV_TO_TEST_RATIO
+            )
+
+            self._save_to_file(train_data, suffix=f"_train_{self.seed}")
+            self._save_to_file(dev_data, suffix=f"_dev_{self.seed}")
+            self._save_to_file(test_data, suffix=f"_test_{self.seed}")
+        else:
+            self._save_to_file(df)
+
+    def _process_file(self, path: Path) -> pd.DataFrame:
+        """
+        Processes the input file and converts it into a DataFrame.
+
+        :param path: The path to the input data file.
+        :return: The processed DataFrame.
+        """
+
+        with open(path) as f:
+            for line in f:
+                self._read_line(line=line)
+            if not self.data.words[-1]:
+                self.data.words = self.data.words[:-1]
+                self.data.tags = self.data.tags[:-1]
+                self.data.spaces_after = self.data.spaces_after[:-1]
+
+        return self.data.to_data_frame()
+
+    def _read_line(self, line: str) -> None:
+        if line.startswith("-DOCSTART-"):
+            return
+
+        stripped_line = line.strip()
+        if not stripped_line:
+            self.data.append_next_lists()
+            return
+
+        try:
+            word, tag, space = stripped_line.split("\t")
+        except ValueError:
+            word, tag = stripped_line.split("\t")
+            space = " "
+        self.data.append(word=word, tag=tag, space=space)
+
+    @staticmethod
+    def _split_dataframe(
+        data: pd.DataFrame, train_prop: float = 0.9
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Splits a DataFrame into training and testing sets based on sentence_id.
+
+        :param data: The DataFrame to split.
+        :param train_prop: The proportion of the data to use for training.
+        :return: A tuple containing the training and testing DataFrames.
+        """
+        sentence_ids = data.sentence_id.unique().tolist()
+        train_count = round(len(sentence_ids) * train_prop)
+        train_ids = random.sample(sentence_ids, train_count)
+
+        train_data = data.loc[data.sentence_id.isin(train_ids)]
+        test_data = data.loc[~data.sentence_id.isin(train_ids)]
+
+        return train_data, test_data
+
+    def _save_to_file(self, data: pd.DataFrame, suffix: str = "") -> None:
+        """
+        Saves the DataFrame to a file.
+
+        :param data: The DataFrame to save.
+        :param out_file: The base name of the output file.
+        :param suffix: The suffix to add to the file name.
+        """
+
+        file_path = self.out_file.parent / f"{self.out_file.name}{suffix}.tsv"
+        data.to_csv(file_path, sep="\t", index=False)
+
+
+# Example usage
+if __name__ == "__main__":
+    kwargs = {"split_to_files": True}
+    files = (
+        (
+            Path("../parsed_data/original_train.conll"),
+            Path("parsed_data/original_train.tsv"),
+        ),
+        (
+            Path("../parsed_data/original_test-A.conll"),
+            Path("parsed_data/original_test-A.tsv"),
+        ),
+        (
+            Path("../parsed_data/text.rest/rest.conll"),
+            Path("parsed_data/text.rest/rest.tsv"),
+        ),
+    )
+    for data_file, out_file in files:
+        kwargs["data_file"] = data_file
+        kwargs["out_file"] = out_file
+
+        splitter = DataFrameSplitter(**kwargs)
+        splitter.run()
--- split_long_examples.py
+++ split_long_examples.py
@@ -1,77 +1,123 @@
 import pandas as pd
-import argparse
 from transformers import AutoTokenizer
+from pathlib import Path
+from typing import Optional
 
 
-def split_long_examples(data, tokenizer, max_seq_len=256, stride=0.8):
-    splitted_data = []
+class DataSplitter:
+    def __init__(
+        self,
+        data_file: Path,
+        tokenizer_path: str,
+        max_seq_len: int = 256,
+        stride: float = 1.0,
+        out_file: Optional[Path] = None,
+    ):
+        """
+        Initializes the DataSplitter with the provided parameters.
 
-    for sentence_id, example in data.groupby("sentence_id"):
-        words_with_labels = []
-        words_in_example = 0
-        tokenized_len = 0
-        token_lens = []
-        chunk_id = 0
+        :param data_file: Path to the input .tsv data file.
+        :param tokenizer_path: Path or name of the tokenizer to use.
+        :param max_seq_len: Maximum sequence length for tokenized words.
+        :param stride: Stride to use when splitting long examples.
+        :param out_file: Path to the output file.
+        """
+        self.data_path = data_file
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+        self.max_seq_len = max_seq_len
+        self.stride = stride
+        self.out_file = out_file or data_file.with_name(
+            f"{data_file.stem}_splitted.tsv"
+        )
 
-        for word, label, space in zip(example.words, example.labels, example.times):
-            tokenized_word = tokenizer.tokenize(word)
-            if tokenized_len + len(tokenized_word) >= max_seq_len - 1:
+    def _split_long_examples(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Splits long examples into smaller chunks based on the maximum sequence length.
+
+        :param data: The input DataFrame containing sentences to split.
+        :return: A DataFrame with the split sentences.
+        """
+        splitted_data = []
+
+        for sentence_id, example in data.groupby("sentence_id"):
+            words_with_labels = []
+            words_in_example = 0
+            tokenized_len = 0
+            token_lens = []
+            chunk_id = 0
+
+            for word, label, space in zip(example.words, example.labels, example.times):
+                tokenized_word = self.tokenizer.tokenize(word)
+                if tokenized_len + len(tokenized_word) >= self.max_seq_len - 1:
+                    splitted_data.extend(
+                        [
+                            (w, l, s, f"{sentence_id}_{chunk_id}")
+                            for w, l, s in words_with_labels
+                        ]
+                    )
+                    chunk_id += 1
+                    offset = int(words_in_example * self.stride)
+                    words_with_labels = words_with_labels[offset:]
+                    tokenized_len -= sum(token_lens[:offset])
+                    token_lens = token_lens[offset:]
+                    words_in_example -= offset
+
+                token_lens.append(len(tokenized_word))
+                tokenized_len += len(tokenized_word)
+                words_with_labels.append((word, label, space))
+                words_in_example += 1
+
+            if tokenized_len >= 0:
                 splitted_data.extend(
                     [
                         (w, l, s, f"{sentence_id}_{chunk_id}")
                         for w, l, s in words_with_labels
                     ]
                 )
-                chunk_id += 1
-                offset = int(words_in_example * stride)
-                words_with_labels = words_with_labels[offset:]
-                tokenized_len -= sum(token_lens[:offset])
-                token_lens = token_lens[offset:]
-                words_in_example -= offset
 
-            token_lens.append(len(tokenized_word))
-            tokenized_len += len(tokenized_word)
-            words_with_labels.append((word, label, space))
-            words_in_example += 1
+        return pd.DataFrame(
+            splitted_data, columns=["words", "labels", "times", "sentence_id"]
+        )
 
-        if tokenized_len >= 0:
-            splitted_data.extend(
-                [
-                    (w, l, s, f"{sentence_id}_{chunk_id}")
-                    for w, l, s in words_with_labels
-                ]
-            )
+    def run(self):
+        """
+        Executes the splitting process and saves the output to the specified file.
+        """
+        data = pd.read_csv(
+            self.data_path,
+            sep="\t",
+            keep_default_na=False,
+            dtype={"words": "str", "labels": "str", "times": "str"},
+        )
 
-    return pd.DataFrame(
-        splitted_data, columns=["words", "labels", "times", "sentence_id"]
-    )
+        splitted_data = self._split_long_examples(data)
+        splitted_data.to_csv(self.out_file, sep="\t", index=False)
 
 
+# Example usage
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--data_path",
-        type=str,
-        default="data.tsv",
-        help="path to .tsv file in simpletransformers train file format",
-    )  # noqa
-    parser.add_argument("--max_seq_len", type=int, default=256)
-    parser.add_argument("--stride", type=float, default=1)
-    parser.add_argument("--out_file", type=str, default="data_splitted.tsv")
-    parser.add_argument(
-        "--tokenizer_path", type=str, default="allegro/herbert-base-cased"
+    tokenizer_path = "allegro/herbert-base-cased"
+    original_train_path = lambda name: Path(
+        f"parsed_data/original_train.tsv_{name}_1353.tsv"
     )
-    args = parser.parse_args()
+    original_test_path = lambda name: Path(
+        f"parsed_data/original_test-A.tsv_{name}_1353.tsv"
+    )
+    rest_path = lambda name: Path(f"parsed_data/text.rest/rest.tsv_{name}_1353.tsv")
+    names = ("dev", "train", "test")
+    path_functions = (original_train_path, original_test_path, rest_path)
+    files = [
+        (path_function(name), path_function(name).with_suffix(".tsv.s"))
+        for name in names
+        for path_function in path_functions
+    ]
 
-    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
-    data = pd.read_csv(
-        args.data_path,
-        sep="\t",
-        keep_default_na=False,
-        dtype={"words": "str", "labels": "str", "times": "str"},
-    )
-
-    data = split_long_examples(
-        data, tokenizer, max_seq_len=args.max_seq_len, stride=args.stride
-    )
-    data.to_csv(args.out_file, sep="\t")
+    for data_file, out_file in files:
+        splitter = DataSplitter(
+            data_file=data_file,
+            tokenizer_path=tokenizer_path,
+            max_seq_len=256,
+            stride=1.0,
+            out_file=out_file,
+        )
+        splitter.run()
